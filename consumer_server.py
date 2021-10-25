0ecf46a846d79d6af9ffd9cba4017f6f144e290c
Semyon Koretskiy <semenmsu@gmail.com>
2021-10-25 15:40:23 +0300

    project

---
+++ consumer_server.py
@@ -0,0 +1,35 @@
+
+import asyncio
+
+from confluent_kafka import Consumer
+
+async def consume(topic_name):
+    consumer = Consumer({
+        'bootstrap.servers': 'PLAINTEXT://localhost:9092',
+        'group.id': '0',
+    })
+    
+    consumer.subscribe([topic_name])
+    
+    while True:
+        messages = consumer.consume()
+        
+        for message in messages:
+            if message is None:
+                print('Message not found')
+            elif message.error() is not None:
+                print(f'Error: {message.error()}')
+            else:
+                print(f'{message.value()}\n')
+                
+        await asyncio.sleep(1.0)
+                
+def consumer():
+    try:
+        asyncio.run(consume('police.service.calls'))
+        
+    except KeyboardInterrupt as e:
+        print("Shutting down...")
+        
+if __name__ == '__main__':
+    consumer()
